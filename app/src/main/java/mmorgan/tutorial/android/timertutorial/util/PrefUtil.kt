ad168b65521018f75b389e16a1ebca183bd121d7
Matthew Morgan <mmorgan55@cnm.edu>
2019-02-25 23:43:59 -0700

    Created PrefUtil class to keep track of timer state and time remaining.

---
+++ app/src/main/java/mmorgan/tutorial/android/timertutorial/util/PrefUtil.kt
@@ -0,0 +1,55 @@
+package mmorgan.tutorial.android.timertutorial.util
+
+import android.content.Context
+import android.preference.PreferenceManager
+import mmorgan.tutorial.android.timertutorial.MainActivity
+
+class PrefUtil {
+    companion object {
+
+        private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID = "com.mmorgan55.timer.previous_timer_length"
+        private const val SECONDS_REMAINING_ID = "com.mmorgan55.timer.previous_timer_length"
+        private const val TIMER_STATE_ID = "com.mmorgan55.timer.timer_state"
+
+        fun getTimerLength(context: Context): Int {
+            return 1
+        }
+
+        fun getPreviousTimerLengthSeconds(context: Context): Long {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, 0)
+        }
+
+        fun setPreviousTimerLengthSeconds(seconds: Long, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, seconds)
+            editor.apply()
+        }
+        
+        fun getTimerState(context: Context): MainActivity.TimerState {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val ordinal = preferences.getInt(TIMER_STATE_ID, 0)
+            return MainActivity.TimerState.values()[ordinal]
+        }
+
+        fun setTimerState(state: MainActivity.TimerState, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val ordinal = state.ordinal
+            editor.putInt(TIMER_STATE_ID, ordinal)
+            editor.apply()
+        }
+
+        fun getSecondsRemaining(context: Context): Long {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(SECONDS_REMAINING_ID, 0)
+        }
+
+        fun setSecondsRemaining(seconds: Long, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(SECONDS_REMAINING_ID, seconds)
+            editor.apply()
+        }
+
+
+    }
+}
